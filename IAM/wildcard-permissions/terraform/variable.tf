d7bf734480c03e41b64f719043d2558c10074d7c
FanoyG <akstyle201471@gmail.com>
2026-01-09 17:46:36 +0530

    Add AWS region and profile variables to variables.tf for wildcard permission lab

---
+++ IAM/wildcard-permissions/terraform/variable.tf
@@ -0,0 +1,12 @@
+
+variable "aws_region" {
+  description = "AWS region for the IAM lab"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile name used by Terraform"
+  type        = string
+  default     = "Adilkhan601782"
+}

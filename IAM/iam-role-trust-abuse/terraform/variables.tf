ef01b69cfc4f1fd300cc825e76f3d197906af95a
FanoyG <akstyle201471@gmail.com>
2026-01-15 20:12:41 +0530

    feat: Add AWS region and Profile for the Lab

---
+++ IAM/iam-role-trust-abuse/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_profile" {
+    description = "AWS CLI profile name used for the lab"
+    type = string
+    default = "Adilkhan601782"
+}
+
+variable "aws_region" {
+    description = "AWS region for the IAM Role Lab"
+    type = string
+    default = "ap-south-1"
+}

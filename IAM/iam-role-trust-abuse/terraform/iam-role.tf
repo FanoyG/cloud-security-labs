fe4218bf1b6efc663ca1aa24f7058ec5ab00d797
FanoyG <akstyle201471@gmail.com>
2026-01-16 00:05:07 +0530

    Fix: File path for assume role policy

--- IAM/iam-role-trust-abuse/terraform/iam-role.tf
+++ IAM/iam-role-trust-abuse/terraform/iam-role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "iam_test_role" {
     name = "s3_bucket_list"
 
-    assume_role_policy = file(path.module + "/trust-policy.json")
+    assume_role_policy = file("${path.module}/trust-policy.json")
 }
